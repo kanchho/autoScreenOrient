dc7df49e777ab4741f4b3a0c78cc22085e91c657
kanchho <bibekji990@gmail.com>
2019-04-06 23:59:24 +0300

    Removed the use of CLI arguments

--- src/daemon/script.py
+++ src/daemon/script.py
@@ -8,12 +8,12 @@ import bluetooth
 import platform
 import win32api as win32 
 import win32con 
+		
 
-class BaseCommunication:
+class BluetoothCommunication():
     def __init__(self):
         self.__endpoint = None
         self.__callback = None
-        self.__extra_parameters = None
 
     def set_endpoint(self, endpoint):
         self.__endpoint = endpoint
@@ -27,16 +27,6 @@ class BaseCommunication:
     def get_callback(self):
         return self.__callback
 
-    def set_extra_parameters(self, parameters):
-        self.__extra_parameters = parameters
-
-    def get_extra_parameters(self):
-        return self.__extra_parameters
-		
-		
-
-class BluetoothCommunication(BaseCommunication):
-
     def listen(self):
         btDevice = self.__connect()
         while True:
@@ -44,7 +34,7 @@ class BluetoothCommunication(BaseCommunication):
             if data == "":
                 continue
             print("received: " + data)
-            self.get_callback()(data, self.get_extra_parameters())
+            self.get_callback()(data)
 
     def __connect(self):
         btDevice = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -54,20 +44,16 @@ class BluetoothCommunication(BaseCommunication):
         return btDevice
 		
 		
-
-endpoint = sys.argv[1]        # provide address of bluetooth device as command line arguments
-reversed = bool(int(sys.argv[2]))      # provide 0 or 1 as command line argument.
-
-def linux(to, reversed):
+def linux(to):
     if to == "0":
         os.system("xrandr --output HDMI-1 --rotate normal")
-    elif (to == "1" and not reversed) or (to == "2" and reversed):
-        os.system("xrandr --output HDMI-1 --rotate right")
-    elif (to == "1" and reversed) or (to == "2" and not reversed):
+    elif to == "1":
         os.system("xrandr --output HDMI-1 --rotate left")
+    elif to == "2":
+        os.system("xrandr --output HDMI-1 --rotate right")
 
 
-def windows(to, reversed):
+def windows(to):
     DISPLAY_NUMBER = 0 # Display number Id of the display to be used with rotation detector
     device = win32.EnumDisplayDevices(None,DISPLAY_NUMBER); 
     print("Rotate device %s (%s)"%(device.DeviceString,device.DeviceName)); 
@@ -75,26 +61,24 @@ def windows(to, reversed):
     dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS) 
     if to == "0":
         dm.DisplayOrientation = win32con.DMDO_DEFAULT 
-    elif (to == "1" and not reversed) or (to == "2" and reversed):
-        dm.DisplayOrientation = win32con.DMDO_270        
-    elif (to == "1" and reversed) or (to == "2" and not reversed):
-        dm.DisplayOrientation = win32con.DMDO_90
+    elif to == "1":
+        dm.DisplayOrientation = win32con.DMDO_90      
+    elif to == "2":
+        dm.DisplayOrientation = win32con.DMDO_270
 		
     dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth 
     win32.ChangeDisplaySettingsEx(device.DeviceName,dm)
 
 
 	
-def switch_display(to, parameters):
-    reversed = parameters['reversed']
+def switch_display(to):
     operating_system = platform.system()
     if (operating_system == "Linux"):
-	    linux(to, reversed)
+	    linux(to)
     elif (operating_system == "Windows"):
-        windows(to, reversed)
+        windows(to)
 
 communicator = BluetoothCommunication()
-communicator.set_endpoint(endpoint)
+communicator.set_endpoint("98:D3:51:F5:AB:13")
 communicator.set_callback(switch_display)
-communicator.set_extra_parameters({'reversed': reversed})
 communicator.listen()
