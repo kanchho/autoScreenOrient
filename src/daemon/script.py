6c13f5cbe42840ea5dcc07e8f7d29edf4d5e9cdf
kanchho <bibekji990@gmail.com>
2019-04-07 18:32:14 +0300

    Code Refactor #1

--- src/daemon/script.py
+++ src/daemon/script.py
@@ -6,8 +6,9 @@ import os
 import sys
 import bluetooth
 import platform
-import win32api as win32 
-import win32con 
+if platform.system() == "Windows":
+    import win32api as win32 
+    import win32con 
 		
 
 class BluetoothCommunication():
@@ -44,26 +45,26 @@ class BluetoothCommunication():
         return btDevice
 		
 		
-def linux(to):
-    if to == "0":
+def linux(receivedData):
+    if receivedData == "0":
         os.system("xrandr --output HDMI-1 --rotate normal")
-    elif to == "1":
+    elif receivedData == "1":
         os.system("xrandr --output HDMI-1 --rotate left")
-    elif to == "2":
+    elif receivedData == "2":
         os.system("xrandr --output HDMI-1 --rotate right")
 
 
-def windows(to):
+def windows(receivedData):
     DISPLAY_NUMBER = 0 # Display number Id of the display to be used with rotation detector
     device = win32.EnumDisplayDevices(None,DISPLAY_NUMBER); 
     print("Rotate device %s (%s)"%(device.DeviceString,device.DeviceName)); 
 
     dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS) 
-    if to == "0":
+    if receivedData == "0":
         dm.DisplayOrientation = win32con.DMDO_DEFAULT 
-    elif to == "1":
+    elif receivedData == "1":
         dm.DisplayOrientation = win32con.DMDO_90      
-    elif to == "2":
+    elif receivedData == "2":
         dm.DisplayOrientation = win32con.DMDO_270
 		
     dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth 
@@ -71,14 +72,18 @@ def windows(to):
 
 
 	
-def switch_display(to):
+def switch_orientation(receivedData):
     operating_system = platform.system()
     if (operating_system == "Linux"):
-	    linux(to)
+	    linux(receivedData)
     elif (operating_system == "Windows"):
-        windows(to)
+        windows(receivedData)
 
-communicator = BluetoothCommunication()
-communicator.set_endpoint("98:D3:51:F5:AB:13")
-communicator.set_callback(switch_display)
-communicator.listen()
+def main():
+    communicator = BluetoothCommunication()
+    communicator.set_endpoint("98:D3:51:F5:AB:13")
+    communicator.set_callback(switch_orientation)
+    communicator.listen()
+
+if __name__ == "__main__":
+    main()
